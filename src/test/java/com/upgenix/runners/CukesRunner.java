a185f61e172167faef561d21be3369f006fd232b
suleysl <97368201+suleysl@users.noreply.github.com>
2022-06-29 23:41:52 +0100

    Framework created

--- src/test/java/com/upgenix/runners/CukesRunner.java
+++ src/test/java/com/upgenix/runners/CukesRunner.java
@@ -10,6 +10,7 @@ import org.junit.runner.RunWith;
             plugin = {"html:target/cucumber-report.html",
                     "rerun:target/rerun.txt",
                     "me.jvt.cucumber.report.PrettyReports:target/cucumber",
+                    "json:target/cucumber.json"
             },
             features = "src/test/resources/features",
             glue = "com/upgenix/step_definitions",
